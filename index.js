6a649f39f847acc3f3a9f6cb899d52d465058cfd
Sahilbhanot7229 <151874618+Sahilbhanot7229@users.noreply.github.com>
2024-05-27 07:36:10 +0100

    structure for backend

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const carRoutes = require('./routes/carRoutes');
+
+app.use('/api/cars', carRoutes);
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
